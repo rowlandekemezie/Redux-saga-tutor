b0875a31b7c5ff03126ab82bfb0d5df4dec4ce9a
Juan Rivillas <juan@viridis-solutions.com>
2017-08-23 15:08:14 -0300

    Fixed errors on webpack and newest version of babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,25 @@
  */
 module.exports = {
   entry: './main.js',
-
-  output: {
-    path: './',
-    filename: 'index.js'
-  },
   devServer: {
     stats: 'error-only',
     inline: true,
     port: 3333
   },
+  output: {
+    path: './',
+    filename: 'index.js'
+  },
   module: {
     loaders: [{
-      test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel',
       query: {
-        presets: ['es2015', 'react', 'stage-2']
+        presets: ['react', 'es2015', 'stage-1']
       }
     }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
   }
 };
